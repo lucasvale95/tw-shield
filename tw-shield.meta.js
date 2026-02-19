1919b664ddecead4de66e8c063d2319c2278fd74
Lucas Vale <lucas.sv95@hotmail.com>
2026-02-19 17:16:58 -0300

    create tw-shield.meta.js

---
+++ tw-shield.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         TW Shield
+// @namespace    tw-shield
+// @version      1.0.0
+// @description  Painel completo para gerenciamento de contas/tribos.
+// @author       TheArrow
+// @match        https://*.tribalwars.com.br/*
+// @updateURL    https://raw.githubusercontent.com/SEU_USUARIO/tw-shield/main/tw-shield.meta.js
+// @downloadURL  https://raw.githubusercontent.com/SEU_USUARIO/tw-shield/main/tw-shield.user.js
+// @connect      uychrksxdelvdmsphzwf.supabase.co
+// @connect      raw.githubusercontent.com
+// @connect      tribalwars.com.br
+// @connect      generativelanguage.googleapis.com
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @grant        GM_info
+// @run-at       document-idle
+// ==/UserScript==
