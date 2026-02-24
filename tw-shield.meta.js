5ad9f04ef6fbccdc86554af121a969b4cc1b580c
Lucas Vale <lucas.sv95@hotmail.com>
2026-02-23 22:44:55 -0300

    Update tw-shield.meta.js

--- tw-shield.meta.js
+++ tw-shield.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TW Shield
 // @namespace    tw-shield
-// @version      1.1.0
+// @version      1.1.1
 // @description  Painel completo para gerenciamento de contas/tribos.
 // @author       TheArrow
 // @match        https://*.tribalwars.com.br/*
