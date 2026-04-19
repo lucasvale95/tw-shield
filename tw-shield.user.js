4e48cad5deba100d41dc90d9d4a31812413eb650
Lucas Vale <lucas.sv95@hotmail.com>
2026-04-19 18:57:02 -0300

    Update tw-shield.user.js

--- tw-shield.user.js
+++ tw-shield.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TW Shield - Source
+// @name         TW Shield
 // @namespace    tw-shield
 // @version      1.1.5
 // @description  Painel completo para gerenciamento de contas/tribos.
@@ -8,6 +8,7 @@
 // @match        https://*.tribalwars.com.pt/*
 // @updateURL    https://raw.githubusercontent.com/lucasvale95/tw-shield/main/tw-shield.meta.js
 // @downloadURL  https://raw.githubusercontent.com/lucasvale95/tw-shield/main/tw-shield.user.js
+// @icon         https://raw.githubusercontent.com/lucasvale95/tw-shield/refs/heads/main/icon.svg
 // @connect      uychrksxdelvdmsphzwf.supabase.co
 // @connect      raw.githubusercontent.com
 // @connect      tribalwars.com.br
